08a25cacb975f7f26304bb1a629663f71b40d754
10xMike <michelegec@gmail.com>
2021-07-11 15:09:38 +0200

    Add names to routes

--- comics/routes/web.php
+++ comics/routes/web.php
@@ -12,42 +12,59 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
-
+/* Home Route */
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+/* Characters Route */
 Route::get('/characters', function () {
     return 'characters page';
-});
+})->name('characters');
 
 /* Comics Route */
 Route::get('/comics', function () {
     $comics = config('comics.data');
     /* ddd($comics); */
     return view('comics', compact('comics'));
-});
+})->name('comics');
 
+/* Movies Route */
 Route::get('/movies', function () {
     return 'movies page';
-});
+})->name('movies');
+
+/* TV Route */
 Route::get('/tv', function () {
     return 'tv page';
-});
+})->name('tv');
+
+/* Games Route */
 Route::get('/games', function () {
     return 'games page';
-});
+})->name('games');
+
+/* Collectibles Route */
 Route::get('/collectibles', function () {
     return 'collectibles page';
-});
+})->name('collectibles');
+
+/* Videos Route */
 Route::get('/videos', function () {
     return 'videos page';
-});
+})->name('videos');
+
+/* Fans Route */
 Route::get('/fans', function () {
     return 'fans page';
-});
+})->name('fans');
+
+/* News Route */
 Route::get('/news', function () {
     return 'news page';
-});
+})->name('news');
+
+/* Shop Route */
 Route::get('/shop', function () {
     return 'shop page';
-});
+})->name('shop');
