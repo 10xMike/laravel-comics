68a8d4ee3cbe06279ca6d392ab239decfd77f196
10xMike <michelegec@gmail.com>
2021-07-11 15:21:52 +0200

    Add $menu items to header.blade.php

--- comics/resources/views/partials/header.blade.php
+++ comics/resources/views/partials/header.blade.php
@@ -1,3 +1,66 @@
+<?php
+$menu = [
+    [
+        'href' => route('home'),
+        'text' => 'Home',
+    ],
+    [
+        'href' => route('characters'),
+        'text' => 'Characters',
+    ],
+    [
+        'href' => route('comics'),
+        'text' => 'Comics',
+    ],
+    [
+        'href' => route('movies'),
+        'text' => 'Movies',
+    ],
+    [
+        'href' => route('tv'),
+        'text' => 'TV',
+    ],
+    [
+        'href' => route('games'),
+        'text' => 'Games',
+    ],
+    [
+        'href' => route('collectibles'),
+        'text' => 'Collectibles',
+    ],
+    [
+        'href' => route('videos'),
+        'text' => 'Videos',
+    ],
+    [
+        'href' => route('fans'),
+        'text' => 'Fans',
+    ],
+    [
+        'href' => route('news'),
+        'text' => 'News',
+    ],
+    [
+        'href' => route('shop'),
+        'text' => 'Shop',
+    ],
+]
+
+?>
+
 <header id="site_header">
-Site Header
+    <div class="topnav">
+        DC POWER ADDITIONAL SITES
+    </div>
+    <div class="main_menu">
+        Main Menu
+    </div>
+    <nav>
+        <ul>
+            @foreach($menu as $item)
+            <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>
+            @endforeach
+        </ul>
+    </nav>
+
 </header>
